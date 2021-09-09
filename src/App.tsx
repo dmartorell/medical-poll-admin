2400ba74fc3fb76a4541cd5f466709a80bc22106
Daniel Martorell <dani.martorell@gmail.com>
2021-09-09 22:20:22 +0200

    Add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      Hello dan
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        {/* <Route exact path="/movie/:id" component={Detail} /> */}
+        <Route exact path="/" component={Home}>
+          <Redirect to="/home" />
+        </Route>
+
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
