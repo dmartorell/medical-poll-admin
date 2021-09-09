345303f33cf3cd5fae797644792b0a6d1aec2f21
Daniel Martorell <dani.martorell@gmail.com>
2021-09-09 22:00:36 +0200

    Set up project with basic dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/semi': 'error',
+
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'no-use-before-define': 'off',
+    'react/prop-types': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
