a534b7c04aab03cc6d70325abfc7514e26b2e8f3
Daniel Martorell <dani.martorell@gmail.com>
2021-09-11 15:46:27 +0200

    Implement Default List fetching and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter, Route, Switch, Redirect,
 } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Patient from './pages/Patient';
 import Project from './pages/Project';
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
       <Switch>
         <Route exact path="/project/:id/:name">
           <Project />
+        </Route>
+        <Route exact path="/patient/:id/">
+          <Patient />
         </Route>
         <Route exact path="/home" component={Home} />
         <Route exact path="/" component={Home}>
---
+++ src/pages/Patient.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Patient = () => (
+  <main>
+    <p>PATIENT PAGE</p>
+  </main>
+    );
+
+export default Patient;
