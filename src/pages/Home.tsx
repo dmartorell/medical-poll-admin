9c9556d78805fa1baf713f0fa03ee51d906ec8ef
Dani Martorell <64319904+dmartorell@users.noreply.github.com>
2021-09-12 18:53:12 +0200

    Add spinner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import React, { FC, useState, useEffect } from 'react';
-import { Stack, Text } from '@chakra-ui/react';
+import { Stack, Spinner, Text } from '@chakra-ui/react';
 import DefaultList from '../components/DefaultList';
 import { fetchDB } from '../helpers/fetchDB';
 import getSurveys from '../helpers/getSurveys';
@@ -9,6 +9,7 @@ const Project: FC = () => {
   const [surveys, setSurveys] = useState<any[]>([]);
 
   useEffect(() => {
+    setSurveys([]);
     fetchDB('answer', undefined, ['patientID', 'project(project_name, id)', 'date'])
       .then((data:any[]) => {
         setSurveys(getSurveys(data).slice(0, 5));
@@ -18,10 +19,22 @@ const Project: FC = () => {
   return (
     <Stack as="main" direction="column" alignItems="center">
       <Stack direction="column" maxWidth="1000px" w="100%" spacing="2em" mt={5}>
-        <Text as="h2" fontSize="4xl" color="blue.700">
+        <Text as="h2" fontSize="3xl" color="blue.700">
           Latest Updates
         </Text>
-        {surveys.length && <DefaultList surveys={surveys} />}
+        {surveys.length
+        ? <DefaultList surveys={surveys} />
+        : (
+          <Stack alignItems="center" justifyContent="center" width="100wv" height="100hv">
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.600"
+              size="xl"
+            />
+          </Stack>
+)}
       </Stack>
     </Stack>
     );
--- src/pages/Patient.tsx
+++ src/pages/Patient.tsx
@@ -1,14 +1,18 @@
-import { Stack, Text } from '@chakra-ui/react';
+import {
+ Stack, Text, Box, HStack, Spinner,
+} from '@chakra-ui/react';
 import React, { FC, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import DefaultList from '../components/DefaultList';
+import { RiFolderUserLine } from 'react-icons/ri';
+import DetailsList from '../components/DetailsList';
 import { fetchDB } from '../helpers/fetchDB';
 import getSurveys from '../helpers/getSurveys';
 
 const Patient: FC = () => {
-  const { id } = (useParams<{id: string}>());
+  const { id, projectName } = (useParams<{id: string, projectName: string}>());
   const [surveys, setSurveys] = useState<any[]>([]);
   useEffect(() => {
+    setSurveys([]);
     fetchDB('answer', `patientID=eq.${id}`, ['patientID', 'date', 'project(project_name, id)'])
       .then((data:any[]) => {
         setSurveys(getSurveys(data));
@@ -17,10 +21,30 @@ const Patient: FC = () => {
   return (
     <Stack as="main" direction="column" alignItems="center">
       <Stack direction="column" maxWidth="1000px" w="100%" spacing="2em" mt={5}>
-        <Text as="h2" fontSize="4xl" color="blue.700">
-          Patient
-        </Text>
-        {surveys.length && <DefaultList surveys={surveys} />}
+        <Stack spacing={0}>
+          <Text as="h3" fontSize="0.9em" color="gray.400">
+            {`PROJECT ${projectName}`}
+          </Text>
+          <HStack alignItems="center" spacing={2}>
+            <Box color="blue.700"><RiFolderUserLine size="30px" /></Box>
+            <Text as="h2" fontSize="3xl" color="blue.700">
+              {`Patient ${id}`}
+            </Text>
+          </HStack>
+        </Stack>
+        {surveys.length
+        ? <DetailsList surveys={surveys} />
+        : (
+          <Stack alignItems="center" justifyContent="center" width="100wv" height="100hv">
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.600"
+              size="xl"
+            />
+          </Stack>
+          )}
       </Stack>
     </Stack>
     );
